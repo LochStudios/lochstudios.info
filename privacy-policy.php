327d3c7f29cef952c907459eb2326b9b3ce54b21
gfaUnDead <contact@gfaundead.stream>
2023-08-09 17:05:47 +1000

    Update privacy-policy.php

--- privacy-policy.php
+++ privacy-policy.php
@@ -1,113 +1,101 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>LochStudios</title>
-		<meta charset="utf-8" />
-		<link rel="icon" href="https://lscdn.site/static/logos/lochstudios/LSLarge.png" type="image/png" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="./assets/css/main.css" />
-	</head>
-	<body class="is-preload">
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>LochStudios | Privacy Policy</title>
+    <link rel="icon" href="https://lscdn.site/static/logos/lochstudios/LSLarge.png" type="image/png" />
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  </head>
+  <body>
 
-		<!-- Header -->
-			<section id="header">
-				<header>
-					<span class="image avatar"><a href="../"><img src="https://lscdn.site/static/logos/lochstudios/black-logo.png" alt="LochStudios Logo" /></a></span>
-					<h1 id="logo">LochStudios</h1>
-					<h5>Hosting with care, all around the world!</h5>
-					<p><a href="mailto:customercare@lochstudios.com">customercare@lochstudios.com</a><p>
-				</header>
-				<nav id="nav">
-					<ul>
-						<li><a href="#pp">Privacy Policy</a></li>
-                        <li><a href="./#about">About Us</a></li>
-						<li><a href="./#info">Our Websites</a></li>
-						<li><a href="./#contact">Contact</a></li>
-						<li><a href="./#faq">FAQ's</a></li>
-					</ul>
-				</nav>
-				<footer>
-					<ul class="icons">
-						<li><a target="_blank" href="https://www.facebook.com/lochstudios/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-						<li><a target="_blank" href="https://twitter.com/LochStudios" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a target="_blank" href="https://www.instagram.com/lochstudiosau/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a target="_blank" href="https://github.com/LochStudios" class="icon brands fa-github"><span class="label">Github</span></a></li>
-						<li><a href="mailto:customercare@lochstudios.com" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
-					</ul>
-				</footer>
-			</section>
+    <!-- Start Top Bar -->
+    <div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+      <div class="title-bar-title">Menu</div>
+    </div>
+    <nav class="top-bar stacked-for-medium" id="mobile-menu">
+      <div class="top-bar-left">
+        <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+          <li class="menu-text">LochStudios</li>
+          <li><a href="../#about">About</a></li>
+          <li><a href="../#websites">Our Websites</a></li>
+          <li><a href="../#contact">Contact</a></li>
+          <li><a href="../#faq">FAQs</a></li>
+          <li class="is-active"><a href="../privacy-policy.php">Privacy Policy</a></li>
+          <li><a href="../terms-conditions.php">Terms & Conditions</a></li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu social-icons">
+          <li><a target="_blank" href="https://www.facebook.com/lochstudios/" class="fab fa-facebook-f"></a></li>
+          <li><a target="_blank" href="https://twitter.com/LochStudios" class="fab fa-twitter"></a></li>
+          <li><a target="_blank" href="https://www.instagram.com/lochstudiosau/" class="fab fa-instagram"></a></li>
+          <li><a target="_blank" href="https://github.com/LochStudios" class="fab fa-github"></a></li>
+          <li><a href="mailto:customercare@lochstudios.com" class="fas fa-envelope"></a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- End Top Bar -->
+	<br>
+    <div class="row">
+      <div class="medium-6 columns medium-push-6">
+        <img src="https://lscdn.site/static/logos/lochstudios/4803721/logo.png">
+      </div>
+      <div class="medium-6 columns medium-pull-6">
+        <h2>LochStudios Privacy Policy</h2>
+		<h3>Who we are:</h3>
+        <p>Our website addresses are:</p>
+		<li>lochstudios.info</li>
+        <li>packages.lochstudios.com</li>
+        <li>media.lochstudios.com</li>
+        <li>getyourwebsite.net.au</li>
+        <li>lscomputerrepair.com</li>
+		<li>fourflavors.net</li>
+		<li>mystorage-dashboard.com</li>
+        <li>sentral.network</li>
+        <li>yourhost.zone</li>
+        <li>yourminecraft.host</li><br>
+      </div>
+    </div>
+	<hr>
+    <div class="row">
+    	<div class="column">
+        	<h3><strong>What personal data we collect and why we collect it</strong></h3>
+        	<h4><strong>Comments</strong></h4>
+        		<p>When visitors leave comments on the site we collect the data shown in the comments form, and also the visitor’s IP address and browser user agent string to help spam detection.
+        		<br>An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it. The Gravatar service privacy policy is available here: <a href="https://automattic.com/privacy/" target="_blank">https://automattic.com/privacy/</a>.
+        		<br>After approval of your comment, your profile picture is visible to the public in the context of your comment.</p>
+        	<h3><strong>Media</strong></h3>
+        		<p>If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS) included. Visitors to the website can download and extract any location data from images on the website.</p>
+        	<h3><strong>Contact forms</strong></h3>
+        	<h4><strong>Cookies</strong></h4>
+        		<p>If you leave a comment on our site you may opt-in to saving your name, email address and website in cookies. These are for your convenience so that you do not have to fill in your details again when you leave another comment. These cookies will last for one year.
+        		<br>If you have an account and you log in to this site, we will set a temporary cookie to determine if your browser accepts cookies. This cookie contains no personal data and is discarded when you close your browser.
+        		<br>When you log in, we will also set up several cookies to save your login information and your screen display choices. Login cookies last for two days, and screen options cookies last for a year. If you select “Remember Me”, your login will persist for two weeks. If you log out of your account, the login cookies will be removed.
+        		<br>If you edit or publish an article, an additional cookie will be saved in your browser. This cookie includes no personal data and simply indicates the post ID of the article you just edited. It expires after 1 day.</p>
+        	<h3><strong>Embedded content from other websites</strong></h3>
+        		<p>Articles on this site may include embedded content (e.g. videos, images, articles, etc.). Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.
+        		<br>These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content, including tracking your interaction with the embedded content if you have an account and are logged in to that website.</p>
+        	<h3><strong>How long we retain your data</strong></h3>
+        		<p>If you leave a comment, the comment and its metadata are retained indefinitely. This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.
+        		<br>For users that register on our website (if any), we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information.</p>
+        	<h3><strong>What rights you have over your data</strong></h3>
+        		<p>If you have an account on any of our sites, or have left comments, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you. This does not include any data we are obliged to keep for administrative, legal, or security purposes.</p>
+		</div>
 
-		<!-- Wrapper -->
-			<div id="wrapper">
+	<hr>
 
-				<!-- Main -->
-					<div id="main">
+    <div class="row column">
+      <ul class="menu">
+      <li>Copyright &copy; <?php echo date("Y"); ?> All rights reserved.<br><strong>LochStudios</strong> and any of <strong>LochStudios</strong> subsidiaries are registered under ABN: <strong>20 447 022 747</strong><br>
+      <strong>LS Computer Repair</strong> is registered under family partnership ABN: <strong>78 341 881 708</strong></li>
+      </ul>
+    </div>
 
-						<!-- One -->
-							<section id="pp">
-								<div class="image main" data-position="center">
-									<img src="./images/banner.jpg" alt="" />
-								</div>
-								<div class="container">
-									<header class="major">
-										<h2>Privacy Policy</h2>
-									</header>
-                                    <div>
-                                        <h3>Who we are</h3>
-                                            Our website addresses are:
-                                            <ul>
-                                                <li>lochstudios.com</li>
-                                                <li>lochstudios.info</li>
-                                                <li>lochs.media</li>
-                                                <li>getyourwebsite.net.au</li>
-                                                <li>lscomputer.repair</li>
-                                                <li>sentral.network</li>
-                                                <li>mystorage-dashboard.com</li>
-                                                <li>yourhost.zone</li>
-                                                <li>yourminecraft.host</li>
-                                            </ul>
-                                        <h3>What personal data we collect and why we collect it</h3>
-                                        <h4>Comments</h4>
-                                        <p>When visitors leave comments on the site we collect the data shown in the comments form, and also the visitor’s IP address and browser user agent string to help spam detection.
-                                        <br>An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it. The Gravatar service privacy policy is available here: <a href="https://automattic.com/privacy/" target="_blank">https://automattic.com/privacy/</a>.
-                                        <br>After approval of your comment, your profile picture is visible to the public in the context of your comment.</p>
-                                        <h3>Media</h3>
-                                        <p>If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS) included. Visitors to the website can download and extract any location data from images on the website.</p>
-                                        <h3>Contact forms</h3>
-                                        <h4>Cookies</h4>
-                                        <p>If you leave a comment on our site you may opt-in to saving your name, email address and website in cookies. These are for your convenience so that you do not have to fill in your details again when you leave another comment. These cookies will last for one year.
-                                        <br>If you have an account and you log in to this site, we will set a temporary cookie to determine if your browser accepts cookies. This cookie contains no personal data and is discarded when you close your browser.
-                                        <br>When you log in, we will also set up several cookies to save your login information and your screen display choices. Login cookies last for two days, and screen options cookies last for a year. If you select “Remember Me”, your login will persist for two weeks. If you log out of your account, the login cookies will be removed.
-                                        <br>If you edit or publish an article, an additional cookie will be saved in your browser. This cookie includes no personal data and simply indicates the post ID of the article you just edited. It expires after 1 day.</p>
-                                        <h3>Embedded content from other websites</h3>
-                                        <p>Articles on this site may include embedded content (e.g. videos, images, articles, etc.). Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.
-                                        <br>These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content, including tracking your interaction with the embedded content if you have an account and are logged in to that website.</p>
-                                        <h3>How long we retain your data</h3>
-                                        <p>If you leave a comment, the comment and its metadata are retained indefinitely. This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.
-                                        <br>For users that register on our website (if any), we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information.</p>
-                                        <h3>What rights you have over your data</h3>
-                                        <p>If you have an account on any of our sites, or have left comments, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you. This does not include any data we are obliged to keep for administrative, legal, or security purposes.</p>
-                                    </div>
-								</div>
-							</section>
-				<!-- Footer -->
-					<section id="footer">
-						<div class="container">
-							<ul class="copyright">
-								<li>Copyright &copy; <?php echo date("Y"); ?> All rights reserved.<br><strong>LochStudios</strong> and any of <strong>LochStudios</strong> subsidiaries are registered under: ABN: <a href="https://abr.business.gov.au/ABN/View/20447022747" target="_blank">20 447 022 747</a><br>
-									<strong>LS Computer Repair</strong> is registered under family partnership ABN: <a href="https://abr.business.gov.au/ABN/View?abn=78341881708" target="_blank">78 341 881 708</a><br>
-								</li>
-							</ul>
-						</div>
-					</section>
-			</div>
-		<!-- Scripts -->
-			<script src="./assets/js/jquery.min.js"></script>
-			<script src="./assets/js/jquery.scrollex.min.js"></script>
-			<script src="./assets/js/jquery.scrolly.min.js"></script>
-			<script src="./assets/js/browser.min.js"></script>
-			<script src="./assets/js/breakpoints.min.js"></script>
-			<script src="./assets/js/util.js"></script>
-			<script src="./assets/js/main.js"></script>
-	</body>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>$(document).foundation();</script>
+  </body>
 </html>
