f9f2cf74ac01094af8910c0eb5a0b7454923ba15
gfaUnDead <contact@gfaundead.stream>
2023-08-09 18:18:23 +1000

    Update terms-conditions.php

--- terms-conditions.php
+++ terms-conditions.php
@@ -38,20 +38,66 @@
       </div>
     </nav>
     <!-- End Top Bar -->
-  <br><section id="about"></section>
+  <br>
     <div class="row">
       <div class="medium-6 columns medium-push-6">
         <img src="https://lscdn.site/static/logos/lochstudios/4803721/logo.png">
       </div>
       <div class="medium-6 columns medium-pull-6">
-        <h3>LochStudios Terms &amp; Conditions</h3>
+        <h3><strong>LochStudios Terms &amp; Conditions</strong></h3>
+        <p>These terms and conditions (the "Terms and Conditions") govern the use of lochstudios.com and any of LochStudios Websites (the "Site").
+          <br>This site is owned and operated by LochStudios.
+          <br>By using this site, you indicate that you have read and understand these Terms and Conditions and agree to abide by them at all times.
+          <br><strong>Effective date: 9th day of August, 2023</strong></p>
       </div>
     </div>
-    <section id="websites"></section>
+    <hr>
     <div class="row">
-        <div class="medium-4 columns">
-
-        </div>
+        <div class="column">
+          <h3><strong>Customer requirements</strong></h3>
+          <h4><strong>The Customer must:</strong></h4>
+          <p>Provide LochStudios with such information and assistance necessary to enable LochStudios to provide the Hosting Services;
+          <br>Have all resources necessary to receive the Hosting Services, including hardware, software, telecommunications resources and internet access acceptable to our network powered by "Mammoth Media Pty Ltd";
+          <br>Not do or attempt to do any of the following as discribed below:
+          <ul>
+          <li>The Customer has not performed or will never perform any illegal or unlawful acts in connection with receipt or use of the Hosting Services</li>
+          <li>To gain unauthorized access to or interfere with any third party's online resources or systems including by any form of hacking.</li>
+          <li>To interfere with any third party's online resources or systems including by carrying out a denial of service attack</li>
+          <li>To distribute, view or create any material that:</li>
+          <ul>
+            <li>Is or may be pornographic, defamatory, offensive, obscene, illegal or unlawful; or</li>
+            <li>Infringes any third party's Intellectual Property Rights or in a way that infringes any third party's Intellectual Property Rights</li>
+            <li>To distribute unsolicited emails to third parties including bulk unsolicited emails</li>
+            <li>To distribute a higher volume of outgoing emails than is acceptable to LochStudios</li>
+            <li>To use a non-existent email return address</li>
+            <li>To use an open email relay</li>
+            <li>In a way that disrupts, misuses or excessively uses the hardware, bandwidth access, storage space or other resources of LochStudios or LochStudios's other customers; or</li>
+            <li>In any other manner that is unacceptable to LochStudios; or</li>
+            <li>Send unsolicited email from another network that appears to have been sent using the Hosting Services or from hardware LochStudios uses to provide the Hosting Services</li>
+          </ul>
+          </ul>
+          If LochSutdios receives a request or notice from a third party (including any regulatory body) requiring LochSutdios to cease providing the Hosting Services to the Customer or remove any content the Customer is making available through use of the Hosting Services LochStudios will comply with lawful instructions in relation to the severity of the incident.
+          <br>You must cooperate with LochStudios, act reasonably and follow LochStudios's directions in connection with this agreement and receipt of the Hosting Services.
+          <br>The Customer acknowledges and agrees that if the Customer does not comply then LochStudios is not obliged to supply the Hosting Services to the Customer.</p>
+          <h3><stong>Backups</stong></h3>
+          <p>The Customer acknowledges and agrees that LochStudios will take backups or otherwise preserve any data or applications stored or used in connection with the Hosting Services on backup servers located offsite to the Hosting Service.</p>
+          <h3><strong>Invoices</strong></h3>
+          <p>LochStudios will invoice the Customer for any and all services that the Customer uses.</p>
+          <p>If the Customer Fails to pay the invoice(s) in accordance with this agreement, without limiting any other remedies available to LochStudios, LochStudios may defer performance of all Hosting Services until the outstanding invoice(s) are paid in full.</p>
+          <h3><strong>Intellectual Property</strong></h3>
+          <p>All content published and made available on our site is the property of LochStudios and the site's creators. This includes, but is not limited to images, text, logos, documents, downloadable files and anything that contributes to the composition of our Site.</p>
+          <h3><strong>Links to Other Websites</strong></h3>
+          <p>Our Site contains links to third party websites or services that we do not own or control. We are not responsible for the content, policies, or practices of any third party website or service linked to on our Site. It is your responsibility to read the terms and conditions and privacy policies of these third party websites before using these sites.</p>
+          <h3><strong>Limitation of Liability</strong></h3>
+          <p>LochStudios and our directors, officers, agents, employees, subsidiaries, and affiliates will not be liable for any actions, claims, losses, damages, liabilities and expenses including legal fees from your use of the Site.</p>
+          <h3><strong>Indemnity</strong></h3>
+          <p>Except where prohibited by law, by using this Site you indemnify and hold harmless LochStudios and our directors, officers, agents, employees, subsidiaries, and affiliates from any actions, claims, losses, damages, liabilities and expenses including legal fees arising out of your use of our Site or your violation of these Terms and Conditions.</p>
+          <h3><strong>Severability</strong></h3>
+          <p>If at any time any of the provisions set forth in these Terms and Conditions are found to be inconsistent or invalid under applicable laws, those provisions will be deemed void and will be removed from these Terms and Conditions. All other provisions will not be affected by the removal and the rest of these Terms and Conditions will still be considered valid.</p>
+          <h3><strong>Changes</strong></h3>
+          <p>These Terms and Conditions may be amended from time to time in order to maintain compliance with the law and to reflect any changes to the way we operate our site and the way we expect users to behave on our site. We will not notify users of changes to these Terms and Conditions or post a notice on our site.</p>
+          <br>
+    </div>
     <hr>
     <div class="row column">
       <ul class="menu">
