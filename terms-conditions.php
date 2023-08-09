3a2848dc2cda7f820e962ba7943130a0c2cd9cfe
gfaUnDead <contact@gfaundead.stream>
2023-08-09 16:57:48 +1000

    Update terms-conditions.php

---
+++ terms-conditions.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>LochStudios | Terms &amp; Conditions</title>
+    <link rel="icon" href="https://lscdn.site/static/logos/lochstudios/LSLarge.png" type="image/png" />
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  </head>
+  <body>
+
+    <!-- Start Top Bar -->
+    <div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+      <div class="title-bar-title">Menu</div>
+    </div>
+    <nav class="top-bar stacked-for-medium" id="mobile-menu">
+      <div class="top-bar-left">
+        <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+          <li class="menu-text">LochStudios</li>
+          <li><a href="index.php/#about">About</a></li>
+          <li><a href="index.php/#websites">Our Websites</a></li>
+          <li><a href="index.php/#contact">Contact</a></li>
+          <li><a href="index.php/#faq">FAQs</a></li>
+          <li><a href="privacy-policy.php">Privacy Policy</a></li>
+          <li class="is-active"><a href="terms-conditions.php">Terms & Conditions</a></li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu social-icons">
+          <li><a target="_blank" href="https://www.facebook.com/lochstudios/" class="fab fa-facebook-f"></a></li>
+          <li><a target="_blank" href="https://twitter.com/LochStudios" class="fab fa-twitter"></a></li>
+          <li><a target="_blank" href="https://www.instagram.com/lochstudiosau/" class="fab fa-instagram"></a></li>
+          <li><a target="_blank" href="https://github.com/LochStudios" class="fab fa-github"></a></li>
+          <li><a href="mailto:customercare@lochstudios.com" class="fas fa-envelope"></a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- End Top Bar -->
+  <br><section id="about"></section>
+    <div class="row">
+      <div class="medium-6 columns medium-push-6">
+        <img src="https://lscdn.site/static/logos/lochstudios/4803721/logo.png">
+      </div>
+      <div class="medium-6 columns medium-pull-6">
+        <h3>LochStudios Terms &amp; Conditions</h3>
+      </div>
+    </div>
+    <section id="websites"></section>
+    <div class="row">
+        <div class="medium-4 columns">
+
+        </div>
+    <hr>
+    <div class="row column">
+      <ul class="menu">
+      <li>Copyright &copy; <?php echo date("Y"); ?> All rights reserved.<br><strong>LochStudios</strong> and any of <strong>LochStudios</strong> subsidiaries are registered under ABN: <strong>20 447 022 747</strong><br>
+      <strong>LS Computer Repair</strong> is registered under family partnership ABN: <strong>78 341 881 708</strong></li>
+      </ul>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>$(document).foundation();</script>
+  </body>
+</html>
