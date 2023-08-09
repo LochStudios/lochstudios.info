cb27945aa882995f62d974844a3f0e350d1a5eda
GFAUnDead <contact@gfaundead.stream>
2023-08-09 15:24:54 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,163 +1,157 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>LochStudios</title>
-		<meta charset="utf-8" />
-		<link rel="icon" href="https://lscdn.site/static/logos/lochstudios/LSLarge.png" type="image/png" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-	</head>
-	<body class="is-preload">
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>LochStudios | Welcome</title>
+    <link rel="icon" href="https://lscdn.site/static/logos/lochstudios/LSLarge.png" type="image/png" />
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  </head>
+  <body>
 
-		<!-- Header -->
-			<section id="header">
-				<header>
-					<span class="image avatar"><a href="../"><img src="https://lscdn.site/static/logos/lochstudios/black-logo.png" alt="LochStudios Logo" /></a></span>
-					<h1 id="logo">LochStudios</h1>
-					<h5>Hosting with care, all around the world!</h5>
-					<p><a href="mailto:customercare@lochstudios.com">customercare@lochstudios.com</a><p>
-				</header>
-				<nav id="nav">
-					<ul>
-						<li><a href="#about">About</a></li>
-						<li><a href="#info">Our Websites</a></li>
-						<li><a href="#contact">Contact</a></li>
-						<li><a href="#faq">FAQ's</a></li>
-						<li><a href="privacy-policy.php#pp">Privacy Policy</a></li>
-					</ul>
-				</nav>
-				<footer>
-					<ul class="icons">
-						<li><a target="_blank" href="https://www.facebook.com/lochstudios/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-						<li><a target="_blank" href="https://twitter.com/LochStudios" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a target="_blank" href="https://www.instagram.com/lochstudiosau/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a target="_blank" href="https://github.com/LochStudios" class="icon brands fa-github"><span class="label">Github</span></a></li>
-						<li><a href="mailto:customercare@lochstudios.com" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
-					</ul>
-				</footer>
-			</section>
+    <!-- Start Top Bar -->
+    <div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+      <div class="title-bar-title">Menu</div>
+    </div>
+    <nav class="top-bar stacked-for-medium" id="mobile-menu">
+      <div class="top-bar-left">
+        <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+          <li class="menu-text">LochStudios</li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#websites">Our Websites</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#faq">FAQs</a></li>
+          <li><a href="privacy-policy.php">Privacy Policy</a></li>
+          <li><a href="terms-conditions.php">Terms & Conditions</a></li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu social-icons">
+          <li><a target="_blank" href="https://www.facebook.com/lochstudios/" class="fab fa-facebook-f"></a></li>
+          <li><a target="_blank" href="https://twitter.com/LochStudios" class="fab fa-twitter"></a></li>
+          <li><a target="_blank" href="https://www.instagram.com/lochstudiosau/" class="fab fa-instagram"></a></li>
+          <li><a target="_blank" href="https://github.com/LochStudios" class="fab fa-github"></a></li>
+          <li><a href="mailto:customercare@lochstudios.com" class="fas fa-envelope"></a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- End Top Bar -->
+  <br><section id="about"></section>
+    <div class="row">
+      <div class="medium-6 columns medium-push-6">
+        <img src="https://lscdn.site/static/logos/lochstudios/4803721/logo.png">
+      </div>
+      <div class="medium-6 columns medium-pull-6">
+        <h2>LochStudios Informational Site</h2>
+        <p>This website has all the information your looking for about the companies that are run under LochStudios. If for any reason you don't find what you're looking for, feel free to give us a call, LochStudios phones run 24 hours a day, 7 days a week!</p>
+      </div>
+    </div>
+    <section id="websites"></section>
+    <div class="row">
+      <div class="medium-4 columns">
+        <h3><div class="stat">9<span> Websites:</span></div></h3><br>
+        <p><strong>Automated Hosting:</strong><br>
+          <a href="https://packages.lochstudios.com/" target="_blank">packages.lochstudios.com</a></p>
+        <p><strong>Multimedia Services:</strong><br>
+          <a href="https://media.lochstudios.com/" target="_blank">media.lochstudios.com</a></p>
+        <p><strong>Website Building Services:</strong><br>
+          <a href="https://getyourwebsite.net.au" target="_blank">getyourwebsite.net.au</a></p>
+        <p><strong>Computer Repair Workshop:</strong><br>
+          <a href="https://lscomputerrepair.com/" target="_blank">lscomputerrepair.com</a></p>
+      </div>
+      <div class="medium-4 columns">
+        <p><strong>Radio Station:</strong><br>
+          <a href="https://www.fourflavors.net/" target="_blank">fourflavors.net</a></p>
+        <p><strong>Storage Panel:</strong><br>
+          <a href="https://mystorage-dashboard.com/" target="_blank">mystorage-dashboard.com</a></p>
+        <p><strong>Gaming Network:</strong><br>
+          <a href="https://sentral.network/" target="_blank">sentral.network</a></p>
+        <p><strong>Hosting Domain:</strong><br>
+          <a href="https://yourhost.zone/" target="_blank">yourhost.zone</a></p>
+        <p><strong>Minecraft Hosting Domain:</strong><br>
+          <a href="https://yourminecraft.host/" target="_blank">yourminecraft.host</a></p>
+      </div>
+      <div class="medium-4 columns">
+        
+      </div>
+    </div>
+    <hr>
+    <section id="contact"></section>
+    <div class="row medium-up-1 large-up-2">
+      <div class="column">
+        <h3><strong>Contact Us</strong></h3>
+        <p><strong>By Mail:</strong> PO Box 219, South Grafton, NSW, 2460, Australia.</p>
+        <p><strong>Our Office Locations:</strong>
+          <li>Level 5, 115 Pitt Street, Sydney, NSW, 2000, Australia.</li>
+          <li>Level 9, 307 Queen Street, Brisbane, QLD, 4000, Australia.</li>
+          <li>Level 6, 607 Bourke Street, Melbourne, VIC, 3000, Australia.</li>
+        </p>
+        <p><strong>Our Phone Numbers:</strong>
+        <br>Switch Board (AU): <a href="tel:+61256323092">+61 (2) 5632-3092</a>
+        <br>Switch Board (NZ): <a href="tel:+6498708886">+64 (9) 870-8886</a>
+        <br>Switch Board (USA): <a href="tel:+13158796488">+1 (315) 879-6488</a>
+        <br>Switch Board (UK): <a href="tel:+442080899548">+44 2080 899 548</a>
+        <br>Remote Support: <a href="tel:1300989567">1300 989 567</a>
+        <br>Short Code: <a href="tel:+611300257727">1300 2 LSPCR (57727)</a>
+        <br>Conference Bridge: <a href="tel:+61256268063">(02) 5626-8063</a>
+        <br>FAX (AU): +61 (2) 5632-3095
+        <br>FAX (USA): +1 (315) 909-6136
+        <br>TEXT MSG/WhatsApp: +614-8000-1064</p>
+      </div>
+      <div class="column">
+        <h4><strong>Direct contact information for our teams:</strong></h4>
+        <p>CUSTOMER CARE: <a href="mailto:customercare@lochstudios.com">customercare@lochstudios.com</a></p>
+        <p><strong>SALES:</strong>
+          <br>Phone: <a href="tel:+61266975683">(02) 6697-5683</a>
+          <br>Email: <a href="mailto:sales@lochstudios.com">sales@lochstudios.com</a></p>
+        <p><strong>SUPPORT</strong>:
+          <br>Phone: <a href="tel:+61266975684">(02) 6697-5684</a>
+          <br>Email: <a href="mailto:support@lochstudios.com">support@lochstudios.com</a></p>
+        <p><strong>MEDIA</strong>:
+          <br>Phone: <a href="tel:+61256546400">(02) 5654-6400</a>
+          <br>Email: <a href="mailto:media@lochstudios.com">media@lochstudios.com</a></p>
+        <h5><strong>LSPCR - Computer Work Shop Contact Information:</strong></h5>
+        <p><strong>Phone:</strong> <a href="tel:+61256112099">(02) 5611-2099</a>
+        <p><strong>Emails:</strong>
+        <br>Office: <a href="mailto:office@lscomputerrepair.com">office@lscomputerrepair.com</a>
+        <br>Support: <a href="mailto:support@lscomputerrepair.com">support@lscomputerrepair.com</a>
+        <br>Billing &amp; Sales: <a href="mailto:billing@lscomputerrepair.com">billing@lscomputerrepair.com</a></p>
+      </div>
+    </div>
+    <hr>
+    <section id="faq"></section>
+    <div class="row">
+        <h3><strong>Frequently Asked Questions:</strong></h3>
+        <p>What have people asked so much, we've put the answers here:
+          <br><br>
+          <strong>Q:</strong> I got a phone call from you, how do I know it's you calling?
+          <br>
+          <strong>A:</strong> We call from one of following numbers:<br>
+                  <strong>Australian Customers: (02) 5632-3092</strong><br>
+                  <strong>United Kingdom Customers: 02080 899 548</strong><br>
+                  <strong>New Zealand Customers: (09) 870-8886</strong><br>
+                  <strong>United States Customers: (315) 879-6488</strong><br>
+                  <strong>If we've called you on your Mobile Phone;<br>We might have called you from this number: 0480001064</strong><br>
+          If you call us back, the first thing you'll hear is our name, LochStudios.<br>"Thanks for calling LochStudios..." That's how you'll know it's us.
+          <br><br>
+          <strong>Q:</strong> You have so many numbers, you don't have a number for what I need, how can I talk to someone?
+          <br>
+          <strong>A:</strong> If you call this direct line to our reception team, they will take your name and number and the right person will call you back: <strong><a href="tel:+61256546402">(02) 5654-6402</a></strong>.<br>
+          Make sure you give them current and corret information, will be in touch with you within 1 business day.
+          </p>
+    </div>
+    <hr>
+    <div class="row column">
+      <ul class="menu">
+      <li>Copyright &copy; <?php echo date("Y"); ?> All rights reserved.<br><strong>LochStudios</strong> and any of <strong>LochStudios</strong> subsidiaries are registered under ABN: <strong>20 447 022 747</strong><br>
+      <strong>LS Computer Repair</strong> is registered under family partnership ABN: <strong>78 341 881 708</strong></li>
+      </ul>
+    </div>
 
-		<!-- Wrapper -->
-			<div id="wrapper">
-
-				<!-- Main -->
-					<div id="main">
-
-						<!-- One -->
-							<section id="about">
-								<div class="image main" data-position="center">
-									<img src="./images/banner.jpg" alt="" />
-								</div>
-								<div class="container">
-									<header class="major">
-										<h2>LochStudios</h2>
-										<p>LOCHSTUDIOS INFORMATION WEBSITE</p>
-									</header>
-									<p>This website has all the information your looking for about the companies that are run under LochStudios.
-										<br>If for any reason you don't find what you're looking for, feel free to give us a call!
-									</p>
-								</div>
-							</section>
-
-						<!-- Information -->
-							<section id="info">
-								<div class="container">
-									<!--<h3>Our Websites!</h3>-->
-									<h3>What are our websites?</h3>
-									<ul class="feature-icons">
-										<li><strong>Automated Hosting:</strong><br><a href="https://packages.lochstudios.com/" target="_blank">packages.lochstudios.com</a></li>
-										<li><strong>Multimedia Services:</strong><br><a href="https://media.lochstudios.com/" target="_blank">media.lochstudios.com</a></li>
-										<li><strong>Website Building Services:</strong><br><a href="https://getyourwebsite.net.au" target="_blank">getyourwebsite.net.au</a></li>
-										<li><strong>Computer Repair Workshop:</strong><br><a href="https://lscomputerrepair.com/" target="_blank">lscomputerrepair.com</a></li>
-										<li><strong>Radio Station:</strong><br><a href="https://www.fourflavors.net/" target="_blank">fourflavors.net</a></li>
-										<li><strong>Storage Panel:</strong><br><a href="https://mystorage-dashboard.com/" target="_blank">mystorage-dashboard.com</a></li>
-										<li><strong>Gaming Network:</strong><br><a href="https://sentral.network/" target="_blank">sentral.network</a></li>
-										<br><br>
-										<li><strong>Hosting Domain:</strong><br><a href="https://yourhost.zone/" target="_blank">yourhost.zone</a></li>
-										<li><strong>Minecraft Hosting Domain:</strong><br><a href="https://yourminecraft.host/" target="_blank">yourminecraft.host</a></li>
-									</ul>
-								</div>
-							</section>
-
-						<!-- Contact -->
-							<section id="contact">
-								<div class="container">
-									<h3>Contact Us</h3>
-									<p>You can contact us with the following details:
-									<br>
-									<br><strong>Mail Contact</strong>: PO Box 219, South Grafton, New South Wales, 2460, Australia.
-									<br><strong>On Location</strong>: Shop 5 - 70 Prince Street, Grafton, New South Wales, 2460, Australia.
-									<br><strong>Switch Board (AU)</strong>: <a href="tel:+61256323092">+61 (2) 5632-3092</a>
-									<br><strong>Switch Board (NZ)</strong>: <a href="tel:+6498708886">+64 (9) 870-8886</a>
-									<br><strong>Switch Board (USA)</strong>: <a href="tel:+13158796488">+1 (315) 879-6488</a>
-									<br><strong>Switch Board (UK)</strong>: <a href="+442080899548">+44 2080 899 548</a>
-									<br><strong>FAX (AU)</strong>: +61 (2) 5632-3095
-									<br><strong>FAX (USA)</strong>: +1 (315) 909-6136
-									<br><strong>TEXT MSG/WhatsApp</strong>: +614-8000-1064
-									<br><strong>Remote Support</strong>: <a href="tel:1300989567">1300 989 567</a>
-									<br><strong>Short Code</strong>: <a href="tel:+611300257727">1300 2 LSPCR (57727)</a>
-									<br><strong>Conference Bridge</strong>: <a href="tel:+61256268063">(02) 5626-8063</a>
-									<br>
-									<br><strong>LSPCR - Computer Work Shop Contact Information:</strong>
-									<br><strong>Address</strong>: Shop 5 - 70 Prince Street, Grafton, New South Wales, 2460, Australia.
-									<br><strong>Phone</strong>: <a href="tel:+61256323092">(02) 5632-3092</a> < Option 1 or Extension 7000
-									<br><strong>Direct Line</strong>: <a href="tel:+61256112099">(02) 5611-2099</a>
-									<br><strong>Emails:</strong>
-									<br><strong>Office</strong>: <a href="mailto:office@lscomputerrepair.com">office@lscomputerrepair.com</a>
-									<br><strong>Support</strong>: <a href="mailto:support@lscomputerrepair.com">support@lscomputerrepair.com</a>
-									<br><strong>Billing&Sales</strong>: <a href="mailto:billing@lscomputerrepair.com">billing@lscomputerrepair.com</a>
-									<br>
-									<br><strong>If you need to contact any of our teams directly, you can contact them via the information below.</strong>
-									<br><br><strong>SALES</strong>: <a href="tel:+61266975683">(02) 6697-5683</a></a><br><strong>Email</strong>: <a href="mailto:sales@lochstudios.com">sales@lochstudios.com</a>
-									<br><br><strong>SUPPORT</strong>: <a href="tel:+61266975684">(02) 6697-5684</a><br><strong>Email</strong>: <a href="mailto:support@lochstudios.com">support@lochstudios.com</a>
-									<br><br><strong>MEDIA</strong>: <a href="tel:+61256546400">(02) 5654-6400</a><br><strong>Email</strong>: <a href="mailto:media@lochstudios.com">media@lochstudios.com</a>
-									<br><br><strong>CUSTOMER CARE</strong>: <a href="mailto:customercare@lochstudios.com">customercare@lochstudios.com</a>
-									</p>
-								</div>
-							</section>
-
-						<!-- FAQ -->
-							<section id="faq">
-								<div class="container">
-									<h3>FAQ's</h3>
-									<p>What have people asked so much, we've put the answers here:
-									<br><br>
-									<strong>Q:</strong> I got a phone call from you, how do I know it's you calling?
-									<br>
-									<strong>A:</strong> We call from one of following numbers:<br>
-														<strong>Australian Customers: (02) 5632-3092</strong><br>
-														<strong>United Kingdom Customers: 02080 899 548</strong><br>
-														<strong>New Zealand Customers: (09) 870-8886</strong><br>
-														<strong>United States Customers: (315) 879-6488</strong><br>
-									If you called one of these numbers back, the first thing you'll hear is our name, LochStudios.<br>"Thanks for calling LochStudios..." That's how you know it's us.
-									<br><br>
-									<strong>Q:</strong> You have so many numbers, you don't have a number for what I need, how can I talk to someone?
-									<br>
-									<strong>A:</strong> If you call this direct line to our reception team, they will take your name and number and the right person will call you back: <strong><a href="tel:+61256546402">(02) 5654-6402</a></strong>.<br>
-									Make sure you give them the corret information and a team member will be in touch with you within 1 business day.
-									</p>
-								</dev>
-							</section>
-					</div>
-
-				<!-- Footer -->
-					<section id="footer">
-						<div class="container">
-							<ul class="copyright">
-								<li>Copyright &copy; <?php echo date("Y"); ?> All rights reserved.<br><strong>LochStudios</strong> and any of <strong>LochStudios</strong> subsidiaries are registered under: ABN: <a href="https://abr.business.gov.au/ABN/View/20447022747" target="_blank">20 447 022 747</a><br>
-									<strong>LS Computer Repair</strong> is registered under family partnership ABN: <a href="https://abr.business.gov.au/ABN/View?abn=78341881708" target="_blank">78 341 881 708</a><br>
-								</li>
-							</ul>
-						</div>
-					</section>
-			</div>
-		<!-- Scripts -->
-			<script src="./assets/js/jquery.min.js"></script>
-			<script src="./assets/js/jquery.scrollex.min.js"></script>
-			<script src="./assets/js/jquery.scrolly.min.js"></script>
-			<script src="./assets/js/browser.min.js"></script>
-			<script src="./assets/js/breakpoints.min.js"></script>
-			<script src="./assets/js/util.js"></script>
-			<script src="./assets/js/main.js"></script>
-	</body>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>$(document).foundation();</script>
+  </body>
 </html>
