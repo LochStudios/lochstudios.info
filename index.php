725007180bce079a3f7e762ee0edf58a9454cccf
GFAUnDead <contact@gfaundead.stream>
2025-08-04 13:18:53 +1000

    Update Bulma and Font Awesome CDN versions
    Upgraded Bulma CSS from v1.0.0 to v1.0.4 and Font Awesome from v5.15.3 to v7.0.0 in index.php, privacy-policy.php, and terms-conditions.php for improved styling and compatibility.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>LochStudios | Welcome</title>
   <link rel="icon" href="4803712.png" type="image/png" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.0/css/bulma.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.4/css/bulma.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
 </head>
 <body>
 
