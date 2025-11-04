068c35e921bdd6e80453c902d9c01c8f7d130a69
gfaUnDead <contact@gfaundead.stream>
2025-11-04 21:38:17 +1100

    Remove footer section from index.php
    Deleted the entire footer HTML block from index.php, including quick links, legal information, social media buttons, and copyright notice.

--- index.php
+++ index.php
@@ -422,57 +422,6 @@ ob_start();
     </div>
   </div>
 </section>
-<footer class="footer has-background-dark">
-  <div class="content has-text-centered has-text-white">
-    <div class="container">
-      <div class="columns is-vcentered">
-        <div class="column">
-          <img src="4803712.png" alt="LochStudios Logo" style="max-height: 60px; margin-bottom: 1rem;">
-        </div>
-      </div>
-      <div class="columns">
-        <div class="column">
-          <h4 class="title is-5 has-text-white">Quick Links</h4>
-          <div class="content">
-            <a href="#about" class="has-text-light">About</a><br>
-            <a href="#websites" class="has-text-light">Our Websites</a><br>
-            <a href="#contact" class="has-text-light">Contact</a><br>
-            <a href="#faq" class="has-text-light">FAQs</a>
-          </div>
-        </div>
-        <div class="column">
-          <h4 class="title is-5 has-text-white">Legal</h4>
-          <div class="content">
-            <a href="privacy-policy.php" class="has-text-light">Privacy Policy</a><br>
-            <a href="terms-conditions.php" class="has-text-light">Terms & Conditions</a>
-          </div>
-        </div>
-        <div class="column">
-          <h4 class="title is-5 has-text-white">Connect With Us</h4>
-          <div class="buttons is-centered">
-            <a target="_blank" href="https://www.facebook.com/lochstudios/" class="button is-light is-rounded">
-              <span class="icon"><i class="fab fa-facebook-f"></i></span>
-            </a>
-            <a target="_blank" href="https://twitter.com/LochStudios" class="button is-light is-rounded">
-              <span class="icon"><i class="fab fa-x-twitter"></i></span>
-            </a>
-            <a target="_blank" href="https://www.instagram.com/lochstudiosau/" class="button is-light is-rounded">
-              <span class="icon"><i class="fab fa-instagram"></i></span>
-            </a>
-            <a target="_blank" href="https://github.com/LochStudios" class="button is-light is-rounded">
-              <span class="icon"><i class="fab fa-github"></i></span>
-            </a>
-          </div>
-        </div>
-      </div>
-      <hr style="background-color: rgba(255,255,255,0.1);">
-      <p class="has-text-grey-light">
-        Copyright &copy; <?php echo date("Y"); ?> All rights reserved.<br>
-        <strong class="has-text-white">LochStudios</strong> and any of <strong class="has-text-white">LochStudios</strong> subsidiaries are registered under ABN: <strong class="has-text-warning">20 447 022 747</strong>.
-      </p>
-    </div>
-  </div>
-</footer>
 <script type="text/javascript">
   document.addEventListener('DOMContentLoaded', () => {
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
